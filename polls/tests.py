e86cb19c509b97e9df9fb8601a1bc6224b0dba51
Melissa Real <melissa.real@hotmail.com>
2019-04-30 23:48:39 +0100

    django tutorial done

---
+++ polls/tests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+# Create your tests here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
